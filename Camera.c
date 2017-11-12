5df3bc535a44502aa098268e70714841909ae1c2
DESKTOP-VKNIFV0\Dude <hypurban86@gmail.com>
2017-11-12 08:33:34 -0500

    Implements basic Camera system

--- Camera.c
+++ Camera.c
@@ -3,3 +3,36 @@
 
 #include "Main.h"
 
+// boolean if Camera is focused on something
+char isFocused = 0;
+
+/* 
+	Pointers to shorts that the Camera should focus on.
+	
+	Notice that we are using POINTERS here...
+	
+	If the x/y position of a worm, weapon, or something else is passed
+	as a focus target as pointers, when that item moves, the Camera's
+	targets will also change (since they're pointers to the original
+	values)
+*/
+short *cameraTargetX;
+short *cameraTargetY;
+
+
+// this is the main update function for the logic of the Camera.
+Camera_update()
+{
+	
+}
+
+// tell the Camera to focus on a pair of X/Y coordinates
+Camera_focusOn(short *targetX, short *target Y)
+{
+	
+}
+
+Camera_clearFocus()
+{
+	
+}
--- headers/Camera.h
+++ headers/Camera.h
@@ -1,2 +1,37 @@
 // Header File
 // Created 11/11/2017; 11:31:35 PM
+
+/*
+	Camera
+	------
+	
+	This is the header file for the Camera system.
+	
+	In the previus version of Worms, the Camera system was rather rigid.
+	
+	The Camera was either:
+		- locked on 1:1 with the worm in control
+		- locked on 1:1 with the weapon
+		- controlled by the user
+		
+	In this new-and-improved Camera system, the camera will have targets
+	that it attempts to follow.
+	
+	It will not immediately lock-on to a target, but rather, it will
+	take it's current position and gradually move towards it's target
+	every frame. This way, the camera will smoothly move between worms
+	/ objects in focus.
+	
+	This header will define some public methods for controlling the camera,
+	as well as the current x/y value of the camera.
+
+*/
+
+// the current position of the camera in game-world units
+int camX = 0;
+int camY = 0;
+
+// function prototypes
+void Camera_update();
+void Camera_focusOn();
+void Camera_clearFocus();
